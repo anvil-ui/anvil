7079e0188a3100f2119d2ed38821a2c3b5c3b745
Kyle Corbitt <kylecorbitt@gmail.com>
2015-06-01 10:49:27 +0100

    RefSet acts as observable

--- library/src/main/java/trikita/anvil/RefSet.java
+++ library/src/main/java/trikita/anvil/RefSet.java
@@ -1,15 +1,23 @@
 package trikita.anvil;
 
+import android.util.Log;
 import android.view.View;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * Created by kyle on 6/1/15.
  */
 public class RefSet {
-    private Map<String, View> refs = new HashMap<String, View>();
+    private Map<String, View> refs = new HashMap();
+    private Map<String, List<RefSetObserver>> observers = new HashMap();
+
+    public interface RefSetObserver {
+        public void update(View v);
+    }
 
     public View get(String ref) {
         return refs.get(ref);
@@ -17,5 +25,17 @@ public class RefSet {
 
     public void set(String ref, View v) {
         refs.put(ref, v);
+        if(null != observers.get(ref))
+            for( RefSetObserver r : observers.get(ref) )
+                r.update(v);
+    }
+
+    public void onUpdate(String ref, RefSetObserver o) {
+        List refObservers = observers.get(ref);
+        if( null == refObservers ) {
+            refObservers = new ArrayList();
+            observers.put(ref, refObservers);
+        }
+        refObservers.add(o);
     }
 }
