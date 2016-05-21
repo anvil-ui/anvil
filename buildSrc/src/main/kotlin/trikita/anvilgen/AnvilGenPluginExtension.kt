55e8f565337d99270d601c0bacc931af15dc2b11
Lalit Maganti <lalitmaganti@gmail.com>
2016-05-21 16:59:22 +0100

    anvilgen: move from Groovy to Kotlin
    While Groovy is considered the de-facto language for Gradle plugin
    development, writing a Kotlin implementation revealed several subtle
    bugs which where being hidden by Groovy and overall seems to make the
    code clearer in most places (with the exception of the ugly closure
    implementations which are unfortunately required to interface with the
    gradle API).
    
    Switch everything over to Kotlin in a manner where no functional changes
    are made. This can be verified by regenerating all the DSLs and checking
    that nothing is changed in that case.

---
+++ buildSrc/src/main/kotlin/trikita/anvilgen/AnvilGenPluginExtension.kt
@@ -0,0 +1,10 @@
+package trikita.anvilgen
+
+open class AnvilGenPluginExtension {
+    var type = ""
+    var libraryName = ""
+    var camelCaseName = ""
+    var version = ""
+    var quirks = mutableMapOf<String, Map<String, Any>>()
+    var dependencies = mutableMapOf<String, List<String>>()
+}
