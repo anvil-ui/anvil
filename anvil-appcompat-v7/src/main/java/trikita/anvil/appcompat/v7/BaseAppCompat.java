2faab16fb6dbf79fd2bd0f3d64436b52de7c5871
gmarques33 <giovannimarques33@gmail.com>
2017-05-02 11:38:09 +0100

    Creating base app compat and base design

---
+++ anvil-appcompat-v7/src/main/java/trikita/anvil/appcompat/v7/BaseAppCompat.java
@@ -0,0 +1,27 @@
+package trikita.anvil.appcompat.v7;
+
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import trikita.anvil.Anvil;
+
+public class BaseAppCompat implements Anvil.AttributeSetter {
+
+  static {
+    Anvil.registerAttributeSetter(new BaseAppCompat());
+  }
+
+  @Override
+  public boolean set(View v, String name, Object value, Object prevValue) {
+    switch (name) {
+      case "layoutGravity":
+        if (v.getLayoutParams() instanceof Toolbar.LayoutParams && value instanceof Integer) {
+          ((Toolbar.LayoutParams) v.getLayoutParams()).gravity = (int) value;
+          return true;
+        }
+        break;
+    }
+
+    return false;
+  }
+
+}
