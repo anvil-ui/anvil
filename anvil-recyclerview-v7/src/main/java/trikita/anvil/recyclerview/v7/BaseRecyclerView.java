46de33281ef5d82765332568d264710e9707cfcd
Sergey Grekov <sngrekov@gmail.com>
2019-08-29 14:39:32 +0300

    fix grid layout and add ability to pass span lookup

--- anvil-recyclerview-v7/src/main/java/trikita/anvil/recyclerview/v7/BaseRecyclerView.java
+++ anvil-recyclerview-v7/src/main/java/trikita/anvil/recyclerview/v7/BaseRecyclerView.java
@@ -2,6 +2,7 @@ package trikita.anvil.recyclerview.v7;
 
 import static trikita.anvil.BaseDSL.attr;
 
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.View;
@@ -23,7 +24,7 @@ class BaseRecyclerView implements Anvil.AttributeSetter {
 
     public static void linearLayoutManager(int orientation, boolean reverseLayout) {
         attr("linearLayoutManager",
-                new LayoutManagerParams(0, orientation, reverseLayout));
+                new LayoutManagerParams(0, orientation, reverseLayout, null));
     }
 
     public static void gridLayoutManager(int spanCount) {
@@ -32,16 +33,32 @@ class BaseRecyclerView implements Anvil.AttributeSetter {
 
     public static void gridLayoutManager(int spanCount, int orientation, boolean reverseLayout) {
         attr("gridLayoutManager",
-                new LayoutManagerParams(spanCount, orientation, reverseLayout));
+                new LayoutManagerParams(spanCount, orientation, reverseLayout, null));
+    }
+
+    public static void gridLayoutManager(int spanCount, int orientation, boolean reverseLayout, GridLayoutManager.SpanSizeLookup spanSizeLookup ) {
+        attr("gridLayoutManager",
+                new LayoutManagerParams(spanCount, orientation, reverseLayout, spanSizeLookup));
     }
 
     @Override
     public boolean set(View v, String name, Object value, Object prevValue) {
-        if (name.equals("linearLayoutManager") || name.equals("gridLayoutManager")) {
+        if (name.equals("linearLayoutManager")) {
             if (v instanceof RecyclerView && value instanceof LayoutManagerParams) {
                 LayoutManagerParams p = (LayoutManagerParams) value;
                 ((RecyclerView) v).setLayoutManager(
                         new LinearLayoutManager(v.getContext(), p.orientation, p.reverseLayout));
+                return true;
+            }
+        }
+        if (name.equals("gridLayoutManager")) {
+            if (v instanceof RecyclerView && value instanceof LayoutManagerParams) {
+                LayoutManagerParams p = (LayoutManagerParams) value;
+                GridLayoutManager lm = new GridLayoutManager(v.getContext(), p.spanCount, p.orientation, p.reverseLayout);
+                if (p.spanSizeLookup != null){
+                    lm.setSpanSizeLookup(p.spanSizeLookup);
+                }
+                ((RecyclerView) v).setLayoutManager(lm);
                 return true;
             }
         }
@@ -52,15 +69,17 @@ class BaseRecyclerView implements Anvil.AttributeSetter {
         private final int spanCount;
         private final int orientation;
         private final boolean reverseLayout;
+        private final GridLayoutManager.SpanSizeLookup spanSizeLookup;
 
-        public LayoutManagerParams(int spanCount, int orientation, boolean reverseLayout) {
+        public LayoutManagerParams(int spanCount, int orientation, boolean reverseLayout, GridLayoutManager.SpanSizeLookup sizeLookup) {
             this.spanCount = spanCount;
             this.orientation = orientation;
             this.reverseLayout = reverseLayout;
+            this.spanSizeLookup = sizeLookup;
         }
 
         public int hashCode() {
-            return (spanCount << 4) + (orientation << 1) + (reverseLayout ? 1 : 0);
+            return (spanCount << 4) + (orientation << 1) + (reverseLayout ? 1 : 0) + spanSizeLookup.hashCode();
         }
 
         public boolean equals(Object o) {
@@ -68,7 +87,7 @@ class BaseRecyclerView implements Anvil.AttributeSetter {
             if (!(o instanceof LayoutManagerParams)) return false;
             LayoutManagerParams params = (LayoutManagerParams) o;
             return params.spanCount == spanCount && params.orientation == orientation &&
-                params.reverseLayout == reverseLayout;
+                params.reverseLayout == reverseLayout && spanSizeLookup == params.spanSizeLookup;
         }
     }
 }
