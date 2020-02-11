dab49f03019ad767706acf9ec91ef3d117f71ddd
sgrekov <sngrekov@gmail.com>
2020-02-11 01:34:49 +0100

    add dsl for autosizing TextView api

--- anvil-appcompat-v7/src/main/kotlin/dev/inkremental/dsl/androidx/appcompat/CustomAppCompatv7Setter.kt
+++ anvil-appcompat-v7/src/main/kotlin/dev/inkremental/dsl/androidx/appcompat/CustomAppCompatv7Setter.kt
@@ -2,19 +2,70 @@
 
 package dev.inkremental.dsl.androidx.appcompat
 
+import android.util.TypedValue.COMPLEX_UNIT_DIP
+import android.util.TypedValue.COMPLEX_UNIT_SP
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.widget.TextViewCompat
 import dev.inkremental.dsl.androidx.appcompat.widget.ToolbarScope
 import dev.inkremental.Inkremental
 import dev.inkremental.attr
+import dev.inkremental.dsl.android.Dip
+import dev.inkremental.dsl.android.Sp
+import dev.inkremental.dsl.androidx.appcompat.widget.AppCompatTextViewScope
 
 fun ToolbarScope.layoutGravity(gravity: Int) = attr("layoutGravity", gravity)
+fun AppCompatTextViewScope.autoSizeTextWithDefaults(autoSizeTextType: Int) = attr("autoSizeTextWithDefaults", autoSizeTextType)
+fun AppCompatTextViewScope.autoSizeTextWithConfiguration(autoSizeMinTextSize: Sp, autoSizeMaxTextSize : Sp, autoSizeStepGranularity : Sp) =
+        attr("autoSizeTextWithConfigurationSp", floatArrayOf(autoSizeMinTextSize.value, autoSizeMaxTextSize.value, autoSizeStepGranularity.value))
+fun AppCompatTextViewScope.autoSizeTextWithConfiguration(autoSizeMinTextSize: Dip, autoSizeMaxTextSize : Dip, autoSizeStepGranularity : Dip) =
+        attr("autoSizeTextWithConfigurationDp", intArrayOf(autoSizeMinTextSize.value, autoSizeMaxTextSize.value, autoSizeStepGranularity.value))
+fun AppCompatTextViewScope.autoSizeTextUniformWithPresetSizesDp(present : IntArray) =
+        attr("autoSizeTextTypeUniformWithPresetSizesDp", present)
+fun AppCompatTextViewScope.autoSizeTextUniformWithPresetSizesSp(present : IntArray) =
+        attr("autoSizeTextTypeUniformWithPresetSizesSp", present)
 
 object CustomAppCompatv7Setter : Inkremental.AttributeSetter<Any> {
     override fun set(v: View, name: String, value: Any?, prevValue: Any?): Boolean = when(name) {
         "layoutGravity" -> when {
             v.layoutParams is Toolbar.LayoutParams && value is Int -> {
                 (v.layoutParams as Toolbar.LayoutParams).gravity = value
+                true
+            }
+            else -> false
+        }
+        "autoSizeTextWithDefaults" -> when {
+            v is TextView && value is Int -> {
+                TextViewCompat.setAutoSizeTextTypeWithDefaults(v, value)
+                true
+            }
+            else -> false
+        }
+        "autoSizeTextWithConfigurationSp" -> when {
+            v is TextView && value is FloatArray -> {
+                TextViewCompat.setAutoSizeTextTypeUniformWithConfiguration(v, value[0].toInt(), value[1].toInt(), value[2].toInt(), COMPLEX_UNIT_SP)
+                true
+            }
+            else -> false
+        }
+        "autoSizeTextWithConfigurationDp" -> when {
+            v is TextView && value is IntArray -> {
+                TextViewCompat.setAutoSizeTextTypeUniformWithConfiguration(v, value[0], value[1], value[2], COMPLEX_UNIT_DIP)
+                true
+            }
+            else -> false
+        }
+        "autoSizeTextTypeUniformWithPresetSizesDp" -> when {
+            v is TextView && value is IntArray -> {
+                TextViewCompat.setAutoSizeTextTypeUniformWithPresetSizes(v, value, COMPLEX_UNIT_DIP)
+                true
+            }
+            else -> false
+        }
+        "autoSizeTextTypeUniformWithPresetSizesSp" -> when {
+            v is TextView && value is IntArray -> {
+                TextViewCompat.setAutoSizeTextTypeUniformWithPresetSizes(v, value, COMPLEX_UNIT_SP)
                 true
             }
             else -> false
